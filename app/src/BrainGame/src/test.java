3b9252fd2120a00bf2645075b320186c77ee2c6d
Co2p <go.co2p@gmail.com>
2014-05-08 15:19:09 +0200

    Initial commit

---
+++ app/src/BrainGame/src/test.java
@@ -0,0 +1,10 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		MathQuiz math = new MathQuiz();
+		
+		
+		
+	}
+}
---
+++ app/src/main/java/com/example/mrbrainy/app/BrainGame.java
@@ -0,0 +1,43 @@
+package com.example.mrbrainy.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+
+
+public class BrainGame extends ActionBarActivity {
+
+    private RadioGroup AnswerGroup;
+    private RadioButton  AnswerButton;
+    private Button btnDisplay;
+    private int pageNumber = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brain_game);
+        String message = this.getResources().getString(R.string.pageNumberM, pageNumber);
+    }
+    public void addListenerOnButton(){
+        AnswerGroup = (RadioGroup) findViewById(R.id.radioAnswerGroup);
+        btnDisplay = (Button) findViewById(R.id.next);
+        btnDisplay.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view){
+                int selectedId = AnswerGroup.getCheckedRadioButtonId();
+                AnswerButton = (RadioButton) findViewById(selectedId);
+                Toast.makeText(BrainGame.this,
+                        AnswerButton.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    protected void incrementPage(){
+
+    }
+}
