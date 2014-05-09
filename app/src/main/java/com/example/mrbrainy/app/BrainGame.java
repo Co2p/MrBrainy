64dd2f86ebf0eb464cd3f4ded0c3e4ac6813f2a3
Co2p <go.co2p@gmail.com>
2014-05-09 22:18:13 +0200

    some temp code

--- app/src/main/java/com/example/mrbrainy/app/BrainGame.java
+++ app/src/main/java/com/example/mrbrainy/app/BrainGame.java
@@ -17,17 +17,28 @@ public class BrainGame extends ActionBarActivity {
     private RadioGroup AnswerGroup;
     private RadioButton  AnswerButton;
     private Button btnDisplay;
+    private String pageString;
+
+    //This needs a object that is compatible with AnswerButton
+    private String realAnswer;
     private int pageNumber = 1;
+    private Mode mode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brain_game);
-        String pageString = new String("Qestion: " + pageNumber);
+        mode = new Mode(5, 3, 2);
+        String pageString = new String("Question nr: " + pageNumber);
         TextView page = (TextView) findViewById(R.id.pageN);
         page.setText(pageString);
 
     }
+
+
+    //When the "next" button is pressed the app should determine if the answer is correct
+    //If it is mode.add should be called, else mode.remove
+    //in both cases the next question needs to be fetched
     public void addListenerOnButton(){
         AnswerGroup = (RadioGroup) findViewById(R.id.radioAnswerGroup);
         btnDisplay = (Button) findViewById(R.id.next);
@@ -36,6 +47,13 @@ public class BrainGame extends ActionBarActivity {
             public void onClick(View view){
                 int selectedId = AnswerGroup.getCheckedRadioButtonId();
                 AnswerButton = (RadioButton) findViewById(selectedId);
+
+                if(AnswerButton.equals(realAnswer)){
+                    mode.add();
+                }
+                else
+                    mode.remove();
+
                 Toast.makeText(BrainGame.this,
                         AnswerButton.getText(), Toast.LENGTH_SHORT).show();
             }
