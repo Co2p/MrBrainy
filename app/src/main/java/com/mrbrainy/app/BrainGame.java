64e15df7ae581996d6f2859f8a334e2969893a63
isidornygren <karlisidornygren@gmail.com>
2014-05-10 19:48:54 +0200

    ändrade lite namn

--- app/src/main/java/com/mrbrainy/app/BrainGame.java
+++ app/src/main/java/com/mrbrainy/app/BrainGame.java
@@ -66,8 +66,8 @@ public class BrainGame extends ActionBarActivity {
             alt1.setText(String.valueOf(quiz.getAnswer()));
             alt2.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
             alt3.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
-            alt4.setText(String.valueOf((quiz.getAnswer())));
-            alt5.setText(String.valueOf((quiz.getAnswer())));
+            alt4.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt5.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
         }
         else if (realAns<2){
             alt1.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
