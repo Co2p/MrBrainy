4a25b4c754d3d03b7017862652a3dcfc9541bb49
Co2p <go.co2p@gmail.com>
2014-05-10 19:02:48 +0200

    re added java files!

---
+++ app/src/main/java/com/mrbrainy/app/BrainGame.java
@@ -0,0 +1,153 @@
+package com.mrbrainy.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Random;
+
+
+public class BrainGame extends ActionBarActivity {
+
+    protected int pageNumber;
+    private int realAns;
+    private MathQuiz quiz;
+    private TextView qText, pageNr, levelText;
+    //AnswerButtons
+    private Button alt1, alt2, alt3, alt4, alt5;
+
+    @Override
+    //Finds the buttons and saves them in variables
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brain_game);
+        quiz = new MathQuiz();
+        pageNumber = 0;
+
+        alt1 = (Button)findViewById(R.id.a1);
+        alt2 = (Button)findViewById(R.id.a2);
+        alt3 = (Button)findViewById(R.id.a3);
+        alt4 = (Button)findViewById(R.id.a4);
+        alt5 = (Button)findViewById(R.id.a5);
+
+        newQuestion();
+    }
+
+    //Generates a new question and adds it to the display
+    protected void newQuestion(){
+        pageNumber++;
+
+        String questionString = quiz.generateQuestion();
+
+        //Displays the question
+        qText = (TextView) findViewById(R.id.question);
+        qText.setText("Vad är: " + questionString + "?");
+
+        //Displays the question number
+        pageNr = (TextView) findViewById(R.id.questionNumber);
+        pageNr.setText("Question " + pageNumber);
+
+        levelText = (TextView) findViewById(R.id.level);
+        levelText.setText("Level " + quiz.getMode().getMode());
+
+        //Randomizes the order of the answers
+        Random altRandomizer = new Random();
+        realAns = altRandomizer.nextInt(5);
+
+        System.out.println(realAns);
+        setButtonText();
+
+    }
+
+    //Randomizes the order of the answers
+    private void setButtonText(){
+        if (realAns<1){
+            alt1.setText(String.valueOf(quiz.getAnswer()));
+            alt2.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt3.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt4.setText(String.valueOf((quiz.getAnswer())));
+            alt5.setText(String.valueOf((quiz.getAnswer())));
+        }
+        else if (realAns<2){
+            alt1.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt2.setText(String.valueOf(quiz.getAnswer()));
+            alt3.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt4.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt5.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+        }
+        else if (realAns<3){
+            alt1.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt2.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt3.setText(String.valueOf(quiz.getAnswer()));
+            alt4.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt5.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+        }
+        else if (realAns<4){
+            alt1.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt2.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt3.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt4.setText(String.valueOf(quiz.getAnswer()));
+            alt5.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+        }
+        else if (realAns<5){
+            alt1.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt2.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt3.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt4.setText(String.valueOf(quiz.getFalseAns(quiz.getAnswer())));
+            alt5.setText(String.valueOf(quiz.getAnswer()));
+        }
+    }
+
+
+    public void button1(View v){
+        if (realAns==0){
+            quiz.getMode().add();
+
+        }
+        else
+            quiz.getMode().remove();
+
+        newQuestion();
+    }
+
+    public void button2(View v){
+        if (realAns==1){
+            quiz.getMode().add();
+        }
+        else
+            quiz.getMode().remove();
+
+        newQuestion();
+    }
+
+    public void button3(View v){
+        if (realAns==2){
+            quiz.getMode().add();
+        }
+        else
+            quiz.getMode().remove();
+
+        newQuestion();
+    }
+
+    public void button4(View v){
+        if (realAns==3){
+            quiz.getMode().add();
+        }
+        else
+            quiz.getMode().remove();
+
+        newQuestion();
+    }
+
+    public void button5(View v){
+        if (realAns==4){
+            quiz.getMode().add();
+        }
+        else
+            quiz.getMode().remove();
+
+        newQuestion();
+    }
+}
---
+++ app/src/main/java/com/mrbrainy/app/Mode.java
@@ -0,0 +1,50 @@
+package com.mrbrainy.app;
+
+public class Mode {
+	//Difficuly
+	private int mode = 0;
+	private int ENHmax;
+	private int correctQ;
+	private int faultMode = 0;
+	private int faultQ;
+	public int ENH = 0;
+	
+	//max är hur många svårighetsgrader det finns (börjar på 0)
+	//och qGoal är hur många frågor som behövs besvaras rätt för att
+	//det ska gå över till nästa svårighetsgrad
+	//falseQ är hur många fel användaren kan svara innan den hoppar tillbaks
+	//ett snäpp i svårighetsgraden
+	public Mode(int maxLevels, int qGoal, int falseQ){
+		ENHmax = maxLevels-1;
+		correctQ = qGoal;
+		faultQ = falseQ;
+	}
+
+	//lägger till för varje korrekt fråga,
+	public boolean add(){
+		faultMode = 0;
+		mode++;
+		if(mode >= correctQ){
+			mode = 0;
+			if(ENH < ENHmax){
+				ENH++;
+                return true;
+			}
+		}
+        return false;
+	}
+
+	//om användaren svarar fel, sätt antal rätt svarade frågor till 0
+	//ändra även så att om avändaren skrivit ett antal fel svar på raken
+	//så gå bakåt ett steg i svårighetsgraden
+	public void remove(){
+		mode = 0;
+		if(faultMode >= faultQ && ENH > 0){
+			ENH--;
+		}
+	}
+	
+	public int getMode(){
+		return ENH+1;
+	}
+}
