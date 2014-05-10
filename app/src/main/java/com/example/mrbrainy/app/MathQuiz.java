bf7bb2070f9015cdcf7d5e726328960fd5b68901
Co2p <go.co2p@gmail.com>
2014-05-10 12:15:26 +0200

    Added questions on division, cleaned up code

--- app/src/main/java/com/example/mrbrainy/app/MathQuiz.java
+++ app/src/main/java/com/example/mrbrainy/app/MathQuiz.java
@@ -7,7 +7,13 @@ public class MathQuiz {
 	//Frågan och svaret
 	private String questionString;
 	private int answer;
+    private Random randomGenerator = new Random();
 	Mode mode;
+    private String plus = new String("+");
+    private String minus = new String("-");
+    private String multiply = new String("*");
+    private String divide = new String("÷");
+
 	
 	public MathQuiz(){
 		mode = new Mode(5, 3, 2);
@@ -17,6 +23,16 @@ public class MathQuiz {
 	public int getAnswer(){
 		return answer;
 	}
+
+    //Generates a faulty answer within a window
+    // (a window that's 10 and answer on 5 returns between 0 and 10)
+    public int getFalseAns(int range){
+        Random randomGenerator = new Random();
+        int randInt;
+        randInt = answer + randomGenerator.nextInt(range) - range/2;
+
+        return randInt;
+    }
 	
 	public String generateQuestion()throws Exception{
         String questionString;
@@ -32,58 +48,56 @@ public class MathQuiz {
 	
 	//Enklaste svårighetsgraden
 	private String easyQ(){
-		Random randomGenerator = new Random();
-		int randInt, var1, var2;
+        int difficulty = 20;
+
+		int randInt, var1, var2, var3;
 		
-		//Olika fr�gor, t.ex. plus, minus och g�nger
-		randInt = randomGenerator.nextInt(2);
+		//Generates a number that decides what mathmatical question to give in the case switch
+		randInt = randomGenerator.nextInt(4);
 		
 		//Generates the basenumbers for each question
-		var1 = randomGenerator.nextInt(20) - 10;
-		var2 = randomGenerator.nextInt(20) - 10;
+        var1 = numGen(difficulty);
+        var2 = numGen(difficulty);
+
 		//The questions
 		switch(randInt){
 			//plus
 			case 0:
 				answer = var1 + var2;
-
-                if(var2<0){
-                    questionString = (var1 + "+" + "(" + var2 + ")");
-                }
-                else
-                    questionString = (var1 + " + " + var2);
-
+                questionString = qString(var1, plus, var2);
 				break;
 			//minus
 			case 1:
 				answer = var1 - var2;
-                if(var2<0){
-                    questionString = (var1 + "-" + "(" + var2 + ")");
-                }
-                else
-				    questionString = (var1 + " - " + var2);
+				questionString = qString(var1, minus, var2);
 
 				break;
 			//multiplication
 			case 2:
 				answer = var1 * var2;
-                if(var2<0){
-                    questionString = (var1 + "*" + "(" + var2 + ")");
-                }
-                else
-				    questionString = (var1 + " * " + var2);
+				questionString = qString(var1, multiply, var2);
 				break;
+            case 3:
+                answer = var1;
+                var3 = var1 * randomGenerator.nextInt(difficulty) - difficulty/2;
+                questionString = qString(var3, divide, var2);
 		}
         return questionString;
 	}
-	
-	//Generates a faulty answer within a window
-	// (a window that's 10 and answer on 5 returns between 0 and 10)
-	public int getFalseAns(int range){
-		Random randomGenerator = new Random();
-		int randInt;
-		randInt = answer + randomGenerator.nextInt(range) - range/2;
-		
-		return randInt;
-	}
+
+    //Returns the question as a string, if the second variable is negative brackets will
+        // be put round it
+    private String qString(int var1, String sign, int var2){
+        if(var2<0){
+            return var1 + sign + "(" + var2 + ")";
+        }
+        else
+            return var1 + sign + var2;
+    }
+
+    //Returns the basenumbers for each question
+    private int numGen(int difficulty){
+        int var = randomGenerator.nextInt(difficulty) - difficulty/2;
+        return var;
+    }
 }
